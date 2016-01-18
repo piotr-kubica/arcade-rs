656564c039b2dfd78e5e2f4dc050f1c3ecd42ba2
Daniel Wiberg <daniel.wiberg@dreambound.se>
2016-01-18 23:39:36 +0100

    We have a window

---
+++ README.md
@@ -0,0 +1,3 @@
+# ArcadeRS
+
+An implementation of the rust [tutorial](https://jadpole.github.io/arcaders/arcaders-1-0/).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    // Initialize sdl2
+    let sdl2_context = sdl2::init().unwrap();
+    let video = sdl2_context.video().unwrap();
+
+    // Create the window
+    let window = video.window("ArcadeRS Shooter", 800, 600)
+        .position_centered()
+        .opengl()
+        .build().unwrap();
+
+    let mut renderer = window.renderer().accelerated().build().unwrap();
+
+    // Render a fully black window
+    renderer.set_draw_color(Color::RGB(0, 0, 0));
+    renderer.clear();
+    renderer.present();
+
+    thread::sleep_ms(3000);
 }
